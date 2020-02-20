b39c5e1dfe0367d42ce83dd84a6f9c2d9d6224e1
AllenSun7 <sunlun7@yahoo.com>
2020-02-20 15:49:24 -0800

    common words count

--- kaggle/random-forest/amazon-alexa-reviews/nlp_sentiment_analysis.py
+++ kaggle/random-forest/amazon-alexa-reviews/nlp_sentiment_analysis.py
@@ -14,6 +14,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+import spacy
+from collections import Counter
 
 def nlp_sentiment_analysis():
     df_review = pd.read_csv('amazon_alexa.tsv', sep='\t')
@@ -30,29 +32,7 @@ def nlp_sentiment_analysis():
     df_review.hist(column='length', by='feedback', bins=50,figsize=(10,4))
     plt.show()
 
-def randome_forest():
-    # Importing the dataset
-    print("preparing data...")
-    dataset = pd.read_csv('amazon_alexa.tsv', delimiter = '\t', quoting = 3)
-    print(dataset.head())
-    print(dataset.describe())
-    row, _ = dataset.shape
-    corpus=[]
-    for i in range(0, row):
-        review = re.sub('[^a-zA-Z]', ' ', dataset['verified_reviews'][i] )
-        review = review.lower()
-        review = review.split()
-        ps = PorterStemmer()
-        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-        review = ' '.join(review)
-        corpus.append(review)
-    
-
-    # creating the Bag of words Model
-    vectorizer = CountVectorizer(max_features=1500)
-    X = vectorizer.fit_transform(corpus).toarray()
-    y = dataset.iloc[:, 0].values
-
+def randome_forest(X, y):
     # Splitting the dataset into the Training set and Test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
@@ -90,10 +70,67 @@ def randome_forest():
     #f1 = f1_score(y_test, y_pred)
     #print("F1-Score: ", f1)
 
+def dataset_visualization():
+ # Importing the dataset
+    print("preparing data...")
+    # Load English tokenizer, tagger, parser, NER and word vectors
+    #nlp = spacy.load("en_core_web_sm")
+    dataset = pd.read_csv('amazon_alexa.tsv', delimiter = '\t', quoting = 3)
+    print(dataset.head())
+    print(dataset.describe())
+    row, _ = dataset.shape
+    corpus=[]
+    # Load English tokenizer, tagger, parser, NER and word vectors
+    #nlp = spacy.load("en_core_web_sm")
+    for i in range(0, row):
+        review = re.sub('[^a-zA-Z]', ' ', dataset['verified_reviews'][i] )
+        review = review.lower()
+        review = review.split()
+        ps = PorterStemmer()
+        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        review = ' '.join(review)
+        #natural language process
+        #doc = nlp(review)
+        #review = [chunk.text for chunk in doc.noun_chunks]
+        #review = ' '.join(review)
+        corpus.append(review)
+    
+
+    # creating the Bag of words Model
+    vectorizer = CountVectorizer(max_features=1500)
+    X = vectorizer.fit_transform(corpus).toarray()
+    y = dataset.iloc[:, 0].values
+    
+    #run random forest
+    #randome_forest(X, y)
+
+    # most common words in negative reviews
+    common_words(corpus)
+
+def common_words(corpus):
+    words = []
+    for i in range(0,len(corpus)):
+        words = words + (re.findall(r'\w+', corpus[i]))
+    # words cantain all the words in the dataset
+    words_counts = Counter(words)
+    print(words_counts)
+    most_common_words = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)
+    most_common_wordList = []
+    most_common_CountList = []
+    for x, y in most_common_words:
+        most_common_wordList.append(x)
+        most_common_CountList.append(y)
+    plt.figure(figsize=(20,18))
+    plot = sns.barplot(np.arange(20), most_common_CountList[0:20])
+    plt.ylabel('Word Count',fontsize=20)
+    plt.xticks(np.arange(20), most_common_wordList[0:20], fontsize=20, rotation=40)
+    plt.title('Most Common Word used in Negative Review.', fontsize=20)
+    plt.show()
 
 def main():
     #nlp_sentiment_analysis()
-    randome_forest()
+    #randome_forest()
+    dataset_visualization()
 
 if __name__ == "__main__":
     main()
