cebbf0bc020e612163fdcdb058f219731f64e051
AllenSun7 <sunlun7@yahoo.com>
2020-02-19 17:07:04 -0800

    For loop returns a smaller errors than GridSearchCV

--- kaggle/random-forest/housing/housing_prediction.py
+++ kaggle/random-forest/housing/housing_prediction.py
@@ -5,6 +5,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+import timeit
+
+"""
+For loop returns a smaller errors than GridSearchCV
+"""
 
 def random_forest():
 
@@ -28,17 +35,62 @@ def random_forest():
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
     X_val = sc.transform(X_val)
+    features = X_train.shape[1]
 
-    # Define the model. Set random_state to 1
-    rf_model = RandomForestRegressor(n_estimators=100, random_state=1)
-    rf_model.fit(X_train, y_train)
-    val_pred = rf_model.predict(X_val)
+    start = timeit.timeit()
+
+    # Define the model. 
+    parameters = {'n_estimators': [100, 200, 300, 400, 500, 600],
+                'max_features': [features-5, features-4, features-3, features-2, features-1, features],
+    }
+
+
+    #train model
+    print("Training model...")
+    rf_model = RandomForestRegressor(random_state=1)
+    grid_obj = GridSearchCV(rf_model, parameters)
+    grid_fit = grid_obj.fit(X_train, y_train)
+    
+    # Get the estimator
+    best_model = grid_fit.best_estimator_
+    val_pred = best_model.predict(X_val)
 
     # Calculate the mean absolute error of your Random Forest model on the validation data
     rf_val_mae = mean_absolute_error(val_pred, y_val)
     # Calculate roc auc
 
     print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+    print("best_model.get_params", best_model.get_params)
+    end = timeit.timeit()
+    print("grid time: %f"% (end-start))
+
+
+    #for loop 
+    start = timeit.timeit()
+    n_estimators = [100, 200, 300, 400, 500, 600]
+    max_features = [features-5, features-4, features-3, features-2, features-1, features]
+    min_error = 1000000
+     
+    for n_estimator in n_estimators:
+        for max_feature in max_features:
+            model = RandomForestRegressor(n_estimators=n_estimator, 
+                                            max_features=max_feature,
+                                            random_state=1)
+            model.fit(X_train, y_train)
+            val_pred = model.predict(X_val)
+            rf_val_mae = mean_absolute_error(val_pred, y_val)
+            #print(f"Validation MAE for Random Forest Model: {rf_val_mae}")
+            #print("model.get_params", model.get_params)
+            if rf_val_mae < min_error:
+                min_error = rf_val_mae
+                fit_model = model
+    
+    print("Validation MAE for Random Forest Model: {}".format(min_error))
+    print("fit_model.get_params", fit_model.get_params)
+    
+    end = timeit.timeit()
+    print("for time: %f" % (end-start))
+
 
 def main():
     random_forest()
